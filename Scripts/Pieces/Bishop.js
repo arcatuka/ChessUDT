b53d39d11019c7a462925a156ea31c673b5e6c3a
Tuka <khangtt1803@gmail.com>
2022-09-07 21:59:13 +0700

    Optimize file

--- Scripts/Pieces/Bishop.js
+++ Scripts/Pieces/Bishop.js
@@ -1,16 +1,5 @@
 import Pieces from "./pieces.js"
 class Bishop extends Pieces{
-    // constructor(UnitName,player,color){
-    //     super();
-    //     this.info=
-    //     {
-    //         UnitName:UnitName,
-    //         color: color,
-    //         isTaken : false,
-    //         urlUnit:null,
-    //     }
-    //     this.player = player;
-    // }
     movement()
     {
         console.log("Bishop is Moving");
--- Scripts/Pieces/Pawn.js
+++ Scripts/Pieces/Pawn.js
@@ -1,16 +1,5 @@
 import Pieces from "./pieces.js"
 class Pawn extends Pieces{
-    // constructor(UnitName,player,color){
-    //     this.info=
-    //     {
-    //         UnitName:UnitName,
-    //         color: color,
-    //         isTaken : false,
-    //         urlUnit:null,
-    //     }
-    //     this.player = player;
-        
-    // }
     movement()
     {
         console.log("Pawn is Moving");
@@ -20,3 +9,6 @@ class Pawn extends Pieces{
         console.log("Pawn is taking a unit");
     }
 }
+
+var a = new Pawn("hello","k","da");
+console.log(a.player);
