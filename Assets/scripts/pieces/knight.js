35beadb442781164b79ada30d74c211f4748cca7
Tuka <khangtt1803@gmail.com>
2022-09-25 19:42:11 +0700

    Fix attribute name

--- Assets/scripts/pieces/knight.js
+++ Assets/scripts/pieces/knight.js
@@ -3,14 +3,12 @@ class Knight extends Piece {
         for (let rowIndex = 0 ; rowIndex < rowList.length ; ++rowIndex) {
             let colList = rowList[rowIndex].colList
             for (let colIndex = 0 ; colIndex < colList.length ; ++colIndex) {
-                const Xposleft = this.rowIndex - rowIndex
-                const Xposright =this.rowIndex + rowIndex
-                const Yposleft = this.colIndex - colIndex
-                const Yposright = this.colIndex + colIndex
+                const rowDistanceValue = this.rowIndex - rowIndex
+                const colDistanceValue = this.colIndex - colIndex
                 const squareObject = rowList[rowIndex].colList[colIndex]
                 const pieceObject = squareObject.piece
                 //console.log(this.rowIndex+ " and "+ rowIndex)
-                if ((Xposleft===2 && Yposleft==1 || Xposleft===2 && Yposleft==-1 || Xposleft===1 && Yposleft==2 || Xposleft===1 && Yposleft==-2 || Xposleft==-2 && Yposleft==1 || Xposleft===-2 && Yposleft==-1|| Xposleft===-1 && Yposleft==-2|| Xposleft===-1 && Yposleft==2) &&pieceObject=== null ) {
+                if ((rowDistanceValue===2 && colDistanceValue==1 || rowDistanceValue===2 && colDistanceValue==-1 || rowDistanceValue===1 && colDistanceValue==2 || rowDistanceValue===1 && colDistanceValue==-2 || rowDistanceValue==-2 && colDistanceValue==1 || rowDistanceValue===-2 && colDistanceValue==-1|| rowDistanceValue===-1 && colDistanceValue==-2|| rowDistanceValue===-1 && colDistanceValue==2) &&pieceObject=== null ) {
                     squareObject.squareElement.style.backgroundColor = 'red'
                 }
             }
